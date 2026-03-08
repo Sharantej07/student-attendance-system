e15bdffd357255662adb7c686c609660c86e4437
Sharantej07 <sharantej.23ise@cambridge.edu.in>
2026-03-08 13:11:58 +0530

    F3-3 Initial attendance feature implementation

--- main.c
+++ main.c
@@ -2,5 +2,8 @@
 //students attendance system demo 
 int main() {
     printf("Student Attendance System\n");
+    printf("1. Mark Attendance\n");
+    printf("2. View Attendance\n");
+    printf("3. Exit\n");
     return 0;
 }
